ff06f6b76f60f2df626d4ec0f04d687836428cf5
Adam Montgomerie <adam@avanssion.com>
2021-09-22 11:07:44 +0900

    add data_path arg

--- data_preprocessing/translate_squad.py
+++ data_preprocessing/translate_squad.py
@@ -9,6 +9,7 @@ def parse_args():
     parser.add_argument("--questions_only", dest="questions_only", action="store_true")
     parser.add_argument("--qa_batch_size", type=int, default=128, required=False)
     parser.add_argument("--context_batch_size", type=int, default=32, required=False)
+    parser.add_argument("--data_path", type=str, default="train-squad.csv", required=False)
     return parser.parse_args()
 
 
@@ -19,7 +20,7 @@ def find_answer_start(row):
 if __name__ == "__main__":
     config = parse_args()
     translator = Translator(config.language)
-    squad_en = pd.read_csv("squad_csv/train-squad.csv")
+    squad_en = pd.read_csv(config.data_path)
     questions = translator.translate(
         squad_en.question,
         batch_size=config.qa_batch_size
