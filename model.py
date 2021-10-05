87433ef2a40db782c797ce6b0caf88607b60bc8f
Adam Montgomerie <adam@avanssion.com>
2021-10-05 19:02:39 +0900

    add TorchModel

--- model.py
+++ model.py
@@ -11,9 +11,9 @@ class ModelOutput:
     loss: torch.Tensor
 
 
-class ChaiiModel(nn.Module):
+class AbhishekModel(nn.Module):
     def __init__(self, model_name: str) -> None:
-        super(ChaiiModel, self).__init__()
+        super(AbhishekModel, self).__init__()
         hidden_dropout_prob: float = 0.0
         layer_norm_eps: float = 1e-7
         config = AutoConfig.from_pretrained(model_name)
@@ -66,5 +66,55 @@ class ChaiiModel(nn.Module):
         loss_fct = nn.CrossEntropyLoss(ignore_index=ignored_index)
         start_loss = loss_fct(start_logits, start_positions)
         end_loss = loss_fct(end_logits, end_positions)
+        total_loss = (start_loss + end_loss) / 2
+        return total_loss
+
+
+class TorchModel(nn.Module):
+    def __init__(self, model_name: str):
+        super(TorchModel, self).__init__()
+        config = AutoConfig.from_pretrained(model_name)
+        self.xlm_roberta = AutoModel.from_pretrained(model_name, config=config)
+        self.qa_outputs = nn.Linear(config.hidden_size, 2)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self._init_weights(self.qa_outputs)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+            if module.bias is not None:
+                module.bias.data.zero_()
+
+    def forward(
+        self,
+        input_ids: torch.Tensor,
+        attention_mask: torch.Tensor,
+        start_positions: torch.Tensor = None,
+        end_positions: torch.Tensor = None
+    ):
+        outputs = self.xlm_roberta(
+            input_ids,
+            attention_mask=attention_mask,
+        )
+
+        sequence_output = outputs[0]
+        qa_logits = self.qa_outputs(sequence_output)
+
+        start_logits, end_logits = qa_logits.split(1, dim=-1)
+        start_logits = start_logits.squeeze(-1)
+        end_logits = end_logits.squeeze(-1)
+
+        loss = None
+        if start_logits is not None and end_logits is not None:
+            loss = self._loss_fn(start_logits, end_logits, start_positions, end_positions)
+
+        return ModelOutput(start_logits=start_logits, end_logits=end_logits, loss=loss)
+
+    def _loss_fn(preds, labels):
+        start_preds, end_preds = preds
+        start_labels, end_labels = labels
+
+        start_loss = nn.CrossEntropyLoss(ignore_index=-1)(start_preds, start_labels)
+        end_loss = nn.CrossEntropyLoss(ignore_index=-1)(end_preds, end_labels)
         total_loss = (start_loss + end_loss) / 2
         return total_loss
